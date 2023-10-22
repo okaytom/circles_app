56c58120a3e5e0fc7171b5e009b2611ddcf1052b
Tommy <tommyojogo123@gmail.com>
2023-10-22 08:57:21 -0400

    Fixed some bugs with the button, now it switches scenes properly

--- src/main/java/com/example/demo/CircleApplication.java
+++ src/main/java/com/example/demo/CircleApplication.java
@@ -3,44 +3,56 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+import javafx.scene.control.Label;
 import java.io.IOException;
+import javafx.scene.control.Button;
 
 public class CircleApplication extends Application {
+    // Main stage for application
     Stage window;
+
+    // Button for adding events
+    Button add_event;
+
+    // Scenes for Calender
+    Scene event_scene;
+    Scene calender_scene;
+
 
     @Override
     public void start(Stage primarystage) throws IOException {
+        // setting main window
         window = primarystage;
-        FXMLLoader fxmlLoader = new FXMLLoader(CircleApplication.class.getResource("hello-view.fxml"));
-
-        //StackPane layout = new StackPane();
-
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
         window.setTitle("Circle");
-        window.setScene(scene);
+        //FXMLLoader fxmlLoader = new FXMLLoader(CircleApplication.class.getResource("hello-view.fxml"));
+
+        // creating layout with buttons and labels
+        VBox layout =  new VBox(30);
+        add_event = new Button("Add event");
+        add_event.setOnAction(e -> AddEvent());
+        layout.getChildren().addAll(add_event);
+
+        calender_scene = new Scene(layout, 640, 480);
+        window.setScene(calender_scene);
+
+
         window.show();
 
-        Label add = new Label("Add event");
-        Button add_event = new Button("Add event");
-        add_event.setOnAction(e -> AddEvent());
+
     }
 
     void AddEvent(){
-        Scene event_scene;
-        Vbox event_layout = new Vbox(20);
-        event_layout.getChildren().addAll(add, add_event);
-        event_scene = new Scene(event_layout, 640, 480);
-        window.setScene(event_scene);
-
-        Vbox event_layout = new Vbox(20);
-        event_layout.getChildren().addAll(add, add_event);
-
+        VBox event_layout = new VBox(20);
         //Go back to calender
         Button calender = new Button("Confirm added event");
-        calender.setOnAction(e -> window.setScene(scene));
+        calender.setOnAction(e -> window.setScene(calender_scene));
+
+        event_layout.getChildren().addAll(calender);
+        event_scene = new Scene(event_layout, 240, 280);
+        window.setScene(event_scene);
+
 
     }
     public static void main(String[] args) {
