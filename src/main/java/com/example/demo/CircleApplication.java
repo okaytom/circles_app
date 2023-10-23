d6309ebb6a837c3e51ce2cb62483419715d0209b
Tommy <tommyojogo123@gmail.com>
2023-10-22 21:22:01 -0400

    Going over to Tanner's branch

--- src/main/java/com/example/demo/CircleApplication.java
+++ src/main/java/com/example/demo/CircleApplication.java
@@ -5,7 +5,6 @@ package com.example.demo;
  */
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
