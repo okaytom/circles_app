61ba7b4e983f8f1ff2b28f58fafc3cdf17e58dfc
Caps <tmandziak02@gmail.com>
2023-11-06 15:57:02 -0600

    Fixed those errors
    I haven't quite figured out scene switching yet, so I'll be working on primarily on a seperate contained project, then when I have something working, I'll move it here
    
    Currently all searchBox does is take what is input into the textfield then output it below after the user presses search, I could also allow them to hit enter as well instead of just the button

--- src/main/java/com/example/demo/SearchController.java
+++ src/main/java/com/example/demo/SearchController.java
@@ -1,4 +1,25 @@
 package com.example.demo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
 public class SearchController {
+    @FXML
+    private Button button_search;
+
+    @FXML
+    private VBox searchOut;
+
+    @FXML
+    private TextField search_field;
+
+    @FXML
+    void onSearchButtonClick(MouseEvent event) {
+        Text t = new Text(search_field.getText());
+        searchOut.getChildren().add(t);
+    }
 }
