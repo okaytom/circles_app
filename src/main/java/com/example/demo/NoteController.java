3cb48493a26dada3aea10b9d3039d81c62a13829
Tanner <tmandziak02@gmail.com>
2023-11-18 16:20:57 -0600

    Added the save() function to NoteController

--- src/main/java/com/example/demo/NoteController.java
+++ src/main/java/com/example/demo/NoteController.java
@@ -5,9 +5,19 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDTrueTypeFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+import java.io.IOException;
+
+//Tanner
 
 public class NoteController {
+    // TODO let the user choose a font size, maybe in menu?
 
     @FXML
     private Menu fileMenu;
@@ -27,5 +37,35 @@ public class NoteController {
     @FXML
     private TextArea textFld;
 
+    // Font for the textArea, frontEnd Font
+    private Font font;
+
+    // Variable for the PDBox font, defaults to time new roman
+    private PDType1Font pdfFont = PDType1Font.TIMES_ROMAN;
+
+    // just a default font size
+    private float fontSize = 10;
+
+
+
+    private void save(String filePath) throws IOException {
+        PDDocument doc = new PDDocument();
+        String txt = textFld.getText();
+        // This line currently gets rid of newLine and invalid characters, as PDFBox does not like them and I haven't found a better fix yet
+        txt = txt.replace("\n", "").replace("\r", "");
+        System.out.println(txt);
+        PDPage page = new PDPage();
+        doc.addPage(page);
+        PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+        contentStream.beginText();
+        // These are currently just filler values for font and layout, I'll do something prettier later
+        contentStream.setFont(pdfFont, fontSize);
+        contentStream.newLine();
+        contentStream.showText(txt);
+        contentStream.endText();
+        contentStream.close();
+        doc.save(filePath);
+        doc.close();
+    }
 
 }
