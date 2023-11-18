783851e9e033db8e0c747e3324d65d30fccbd236
Tanner <tmandziak02@gmail.com>
2023-11-18 16:07:09 -0600

    Added a noteView and NoteController class
    noteView isn't anything crazy now, just an early start

---
+++ src/main/java/com/example/demo/NoteController.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class NoteController {
+
+    @FXML
+    private Menu fileMenu;
+
+    @FXML
+    private Menu fonntMenu;
+
+    @FXML
+    private MenuItem loadBtn;
+
+    @FXML
+    private MenuBar menuBr;
+
+    @FXML
+    private MenuItem saveBtn;
+
+    @FXML
+    private TextArea textFld;
+
+
+}
