9dcb9ee10535539025e32ddac74724785b4c1f3c
Tommy <tommyojogo123@gmail.com>
2023-11-23 09:51:15 -0500

    pulled most recent version from main and added subject page which allows the user to select, add and remove subjects. Also made the buttons 'circles'

---
+++ src/main/java/com/example/demo/TextBox.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class TextBox {
+    //TODO what happens when users closes the textbox?
+    static String userText;
+    public static String display(String title, String message){
+        Stage text_window = new Stage();
+
+
+        // have to address this window before any other open window
+        text_window.initModality(Modality.APPLICATION_MODAL);
+        text_window.setTitle(title);
+        text_window.setMinWidth(350);
+
+        Label text = new Label();
+        text.setText(message);
+
+        Button yes = new Button("Okay");
+        yes.setOnAction(e -> {
+            text_window.close();
+        });
+
+        TextField input = new TextField();
+
+        Button okay = new Button("Okay");
+        okay.setOnAction(e -> {
+            if(input.getText().isBlank()){
+                AlertBox.display("Invalid Input", "Need to input text");
+            }
+            else{
+                userText = input.getText();
+                text_window.close();
+            }
+        });
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(text, input, okay);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene text_scene = new Scene(layout);
+        text_window.setScene(text_scene);
+        text_window.showAndWait(); // needs to be closed
+
+        return userText;
+    }
+}
